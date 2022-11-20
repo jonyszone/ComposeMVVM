a548f41beb3e0631da5054c930d2ad2f7aae1f39
Shafi <codewithshafi@gmail.com>
2022-11-20 14:32:11 +0600

    column row practices

--- app/src/main/java/com/example/composemvvm/MainActivity.kt
+++ app/src/main/java/com/example/composemvvm/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.composemvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -25,19 +25,52 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .border(border = BorderStroke(width = 1.dp, color = Color.Red)),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Item 1 ",
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Row(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(200.dp)
+                        .border(border = BorderStroke(width = 1.dp, color = Color.Red))
+                ) {
+                    Text(
+                        text = "Item 1 ",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+            }
+
+        }
+
+        /*setContent {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color.Gray)
+                    .background(color = Color(0xFFE5E4E2))
                     .verticalScroll(rememberScrollState())
 
 
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.burger),
-                    contentDescription = "",
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
+                    contentDescription = null,
+                    modifier = Modifier.clip(RoundedCornerShape(10))
+                        .height(300.dp),
+                    contentScale = ContentScale.Crop,
                 )
 
                 Column(modifier = Modifier.padding(16.dp)) {
@@ -61,7 +94,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
-        }
+        }*/
     }
 }
 
