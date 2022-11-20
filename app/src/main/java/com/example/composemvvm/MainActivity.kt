c26bc21ae2acfc2f438220d36ec40b16e5a29969
Shafi <codewithshafi@gmail.com>
2022-11-20 12:54:22 +0600

    layout practices

--- app/src/main/java/com/example/composemvvm/MainActivity.kt
+++ app/src/main/java/com/example/composemvvm/MainActivity.kt
@@ -3,31 +3,65 @@ package com.example.composemvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.example.composemvvm.ui.theme.ComposeMVVMTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Hey look some text")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = { }) {
-                    Text(text = "A Button")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Gray)
+                    .verticalScroll(rememberScrollState())
+
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.burger),
+                    contentDescription = "",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Happy Meal",
+                        fontSize = 26.sp
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "800 Calories",
+                        fontSize = 16.sp
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$5",
+                        style = TextStyle(
+                            color = Color(0xFF85bb65),
+                            fontSize = 16.sp
+                        ),
+                    )
                 }
             }
+
         }
     }
 }
+
