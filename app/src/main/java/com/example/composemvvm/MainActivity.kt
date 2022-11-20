24b097a5fcfb348c8996c0889fcfe35db95c190c
Shafi <codewithshafi@gmail.com>
2022-11-20 11:26:39 +0600

    testing app: working

--- app/src/main/java/com/example/composemvvm/MainActivity.kt
+++ app/src/main/java/com/example/composemvvm/MainActivity.kt
@@ -3,41 +3,31 @@ package com.example.composemvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composemvvm.ui.theme.ComposeMVVMTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = { }) {
+                    Text(text = "A Button")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMVVMTheme {
-        Greeting("Android")
     }
 }
